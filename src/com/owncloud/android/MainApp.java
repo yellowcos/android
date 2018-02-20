d3d227372ca446bc1b8f8347ef770edd4ae7b502
davigonz <dgonzalez@solidgear.es>
2018-02-20 18:45:18 +0100

    Update some dates and minor changes

--- src/com/owncloud/android/MainApp.java
+++ src/com/owncloud/android/MainApp.java
@@ -169,10 +169,7 @@ public class MainApp extends Application {
     public static String getAccountType() {
         return getAppContext().getResources().getString(R.string.account_type);
     }
-
-
-    // Non gradle build systems do not provide BuildConfig.VERSION_CODE
-    // so we must fallback to this method :(
+    
     public static int getVersionCode() {
         try {
             String thisPackageName = getAppContext().getPackageName();
@@ -182,8 +179,6 @@ public class MainApp extends Application {
         }
     }
 
-    //  From AccountAuthenticator 
-    //  public static final String AUTHORITY = "org.owncloud";
     public static String getAuthority() {
         return getAppContext().getResources().getString(R.string.authority);
     }
